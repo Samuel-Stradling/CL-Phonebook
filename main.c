858b230a3fe27810fcf9a8fa15ec9c26c27ac6ef
Macbook air <sammystradling2006@gmail.com>
2023-03-04 19:02:14 +0000

    readall functionality working

--- main.c
+++ main.c
@@ -1,6 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(){
+int read_all(FILE *file, const char* delimiter){
+    char buffer[1024];
+    int row_count = 0;
+    int field_count = 0;
 
-	return 0;
+    file = fopen("data.csv", "r");
+    if (file == NULL) {
+        printf("Could not open file, trying again\n");
+
+        fclose(file);
+        return -1;
+    }
+
+    // reads data from file stream. stores in buffer. reads 1024 characters (size of buffer).
+    // reads from 'file'
+    while (fgets(buffer, 1024, file)) {
+        row_count++;
+
+        char* field = strtok(buffer, delimiter);
+
+
+        //while there is a field (data delimited by delimiter)
+
+        printf("Contact %d:\n", row_count);
+        while (field) {
+            // runs until eol
+            printf(" %s\n", field);
+            //gets the next token in the line: next csv record
+            field = strtok(NULL, delimiter);
+
+        }
+    }
+    fclose(file);
+    return 0;
+}
+
+
+
+int main() {
+    // creates variable 'file' of type pointer to a FILE
+    FILE *file;
+    const char* delimiter = ",";
+
+    int read = read_all(file, delimiter);    
+    while (read==-1){
+        read = read_all(file, delimiter);
+    }
+
+
+    return 0;
 }
